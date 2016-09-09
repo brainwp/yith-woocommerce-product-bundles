213ecc43958cd585edd141c685a5ae1fea80007e
matheusgimenez <contato@matheusgimenez.com.br>
2016-09-09 16:55:22 -0300

    fazendo fork & fixando sumiço da aba geral na edição de produtos

---
+++ plugin-options/how-to-options.php
@@ -0,0 +1,12 @@
+<?php
+// Exit if accessed directly
+! defined( 'YITH_WCPB' )  && exit();
+
+return array(
+	'how-to' => array(
+		'how-to' => array(
+			'type' => 'custom_tab',
+			'action' => 'yith_wcpb_how_to_tab'
+		)
+	)
+);
---
+++ templates/how-to.php
@@ -0,0 +1,61 @@
+<style>
+    .yith-wcbep-how-to-content {
+        padding    : 5px 20px 20px 20px;
+        background : #fff;
+    }
+
+    .yith-wcbep-how-to-content img {
+        width         : 100%;
+        max-width     : 700px;
+        box-shadow    : 0 2px 10px 0 #ccc;
+        border        : 1px solid #ccc;
+    }
+
+</style>
+
+<div class="yith-wcbep-how-to-content">
+    <p>
+        <?php echo sprintf( __( 'This page of the documentation is a guide for the usage of the free version of the %sYITH WooCommerce Product Bundles%s plugin.', 'yith-woocommerce-product-bundles' ), '<strong>', '</strong>' ) ?>
+        <br/>
+        <?php _e( 'Now we analyze the necessary steps for the cretion of a product bundle, and its possible options.', 'yith-woocommerce-product-bundles' ) ?>
+        <br/>
+        <br/>
+        <?php echo sprintf( __( 'Firstly, create a new product on "WooCommerce" and select the %sProduct Bundle%s type in the product detail page.', 'yith-woocommerce-product-bundles' ), '<strong>', '</strong>' ) ?>
+    </p>
+    <img src="<?php echo YITH_WCPB_ASSETS_URL ?>/images/how-to/01.jpg" alt="bundle-01"/>
+
+    <p>
+        <?php echo sprintf( __( 'Now configure the options clickin on the new tab "%1$sBundle Options%2$s" that has been added to the "%1$sProduct Data%2$s" section of the product.', 'yith-woocommerce-product-bundles' ), '<strong>', '</strong>' ) ?>
+    </p>
+    <img src="<?php echo YITH_WCPB_ASSETS_URL ?>/images/how-to/02.jpg" alt="bundle-02"/>
+
+    <p>
+        <?php _e( 'Add the products you want to add in the budle using the related Ajax search field.', 'yith-woocommerce-product-bundles' ) ?>
+    </p>
+    <img src="<?php echo YITH_WCPB_ASSETS_URL ?>/images/how-to/03.jpg" alt="bundle-03"/>
+
+    <p>
+        <?php echo sprintf( __( '%1$sNB: the plugin lets you add only simple products.%2$s', 'yith-woocommerce-product-bundles' ), '<strong>', '</strong>' ) ?>
+        <br />
+        <?php _e( 'Select the quantity of the products to be added in the bundle.', 'yith-woocommerce-product-bundles' ) ?>
+    </p>
+    <img src="<?php echo YITH_WCPB_ASSETS_URL ?>/images/how-to/04.jpg" alt="bundle-04"/>
+
+    <p>
+        <?php echo sprintf( __( 'Now you just have to set the price, the description and the image of your product bundle with the %sGeneral%s option and you will be done.', 'yith-woocommerce-product-bundles' ), '<strong>', '</strong>' ) ?>
+    </p>
+    <img src="<?php echo YITH_WCPB_ASSETS_URL ?>/images/how-to/05.jpg" alt="bundle-05"/>
+
+    <p>
+        <?php _e( 'Here you are an example of a "Bundle" product.', 'yith-woocommerce-product-bundles' ) ?>
+    </p>
+    <img src="<?php echo YITH_WCPB_ASSETS_URL ?>/images/how-to/06.jpg" alt="bundle-06"/>
+
+    <p>
+        <?php _e( 'As you can see, the available products of the bundle have been added in the selected quantity.', 'yith-woocommerce-product-bundles' ) ?>
+    </p>
+
+    <p>
+        <a href="//yithemes.com/docs-plugins/yith-woocommerce-product-bundles/"><?php _e('View plugin documentation', 'yith-woocommerce-product-bundles') ?></a>
+    </p>
+</div>
